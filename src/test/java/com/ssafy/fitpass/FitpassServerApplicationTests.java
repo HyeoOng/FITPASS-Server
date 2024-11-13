8177904167856138d81e2543c60f803a89dbbb70
ImJiyun <jiyunk920@gmail.com>
2024-11-13 16:49:15 +0900

    first commit

---
+++ src/test/java/com/ssafy/fitpass/FitpassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fitpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitpassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
