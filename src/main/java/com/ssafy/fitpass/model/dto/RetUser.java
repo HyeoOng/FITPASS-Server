d9585e4c45b76a0093cae9477b7c5628ddbc37ec
나혜원 <n417759@gmail.com>
2024-11-14 18:51:20 +0900

    feat : add retUser dto

---
+++ src/main/java/com/ssafy/fitpass/model/dto/RetUser.java
@@ -0,0 +1,102 @@
+package com.ssafy.fitpass.model.dto;
+
+import java.util.Date;
+
+public class RetUser {
+    public int userId;
+    public String email, name, nickname;
+    public int gender;
+    public Date birthdate, regDate;
+    public int admin;
+    public String profile;
+
+    public RetUser() {}
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+    public void setAdmin(int admin) {
+        this.admin = admin;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    @Override
+    public String toString() {
+        return "RetUser{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", gender=" + gender +
+                ", birthdate=" + birthdate +
+                ", regDate=" + regDate +
+                ", admin=" + admin +
+                ", profile='" + profile + '\'' +
+                '}';
+    }
+}
+
