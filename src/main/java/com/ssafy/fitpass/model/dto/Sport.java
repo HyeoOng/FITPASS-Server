95a0af98682c892fb58a4e6dec4cf571253e3e82
ImJiyun <jiyunk920@gmail.com>
2024-11-14 18:49:37 +0900

    Feat: Post, Sport, User class 구현

--- src/main/java/com/ssafy/fitpass/model/dto/Sport.java
+++ src/main/java/com/ssafy/fitpass/model/dto/Sport.java
@@ -1,4 +1,43 @@
 package com.ssafy.fitpass.model.dto;
 
 public class Sport {
+    private int sportCode;
+    private String sportName;
+
+    public Sport() {
+    }
+
+    public Sport(String sportName) {
+        setSportName(sportName);
+    }
+
+    public Sport(int sportCode, String sportName) {
+        setSportCode(sportCode);
+        setSportName(sportName);
+    }
+
+    // getter와 setter
+    public int getSportCode() {
+        return sportCode;
+    }
+
+    public void setSportCode(int sportCode) {
+        this.sportCode = sportCode;
+    }
+
+    public String getSportName() {
+        return sportName;
+    }
+
+    public void setSportName(String sportName) {
+        this.sportName = sportName;
+    }
+
+    @Override
+    public String toString() {
+        return "Sport{" +
+                "sportCode=" + sportCode +
+                ", sportName='" + sportName + '\'' +
+                '}';
+    }
 }
