270b01e99e4a1e7a60bfb1af0616d6bb1c2c8487
ImJiyun <jiyunk920@gmail.com>
2024-11-16 19:58:31 +0900

    feat: add Dao, Dto classes, and xml file about Admin Request

---
+++ src/main/java/com/ssafy/fitpass/model/dao/AdminRequestDao.java
@@ -0,0 +1,21 @@
+package com.ssafy.fitpass.model.dao;
+
+import com.ssafy.fitpass.model.dto.AdminRequest;
+
+public interface AdminRequestDao {
+    /**
+     * 관리자에게 요청을 보내는 메서드입니다.
+     *
+     * @param adminRequest
+     * @return 등록된 행 수 (등록 성공 : 1)
+     */
+    int insertRequest(AdminRequest adminRequest);
+
+    /**
+     * 요청을 삭제하는 메서드입니다.
+     *
+     * @param requestId
+     * @return 삭제된 행 수 (삭제 성공 : 1)
+     */
+    int deleteRequest(int requestId);
+}
