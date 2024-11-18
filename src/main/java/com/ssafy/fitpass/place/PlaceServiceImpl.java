e23671a59838ff37808734dfb7ebf7bfc6f94008
나혜원 <n417759@gmail.com>
2024-11-18 15:24:35 +0900

    fix: fix the placeService

--- src/main/java/com/ssafy/fitpass/place/PlaceServiceImpl.java
+++ src/main/java/com/ssafy/fitpass/place/PlaceServiceImpl.java
@@ -18,18 +18,5 @@ public class PlaceServiceImpl implements PlaceService {
         return placeDao.selectAll(userId);
     }
 
-    @Override
-    public Place getPlace(int postId) {
-        return placeDao.selectOne(postId);
-    }
 
-    @Override
-    public int getPlaceId(int kakaoMapId) {
-        return placeDao.selectOneByKakaoMapId(kakaoMapId);
-    }
-
-    @Override
-    public boolean createPlace(Place place) {
-        return placeDao.insertPlace(place)==1;
-    }
 }
