493b1d128fd1d55b0e4cdd76aa260eac53080c53
ImJiyun <jiyunk920@gmail.com>
2024-11-19 20:29:22 +0900

    fix: fix origin url and delete profile in xml

--- src/main/java/com/ssafy/fitpass/config/WebConfig.java
+++ src/main/java/com/ssafy/fitpass/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.ssafy.fitpass.config;
 
 import com.ssafy.fitpass.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 오리진 요청을 열어줘야함 (포트 주의)
+                .allowedOrigins("http://localhost:5174") // 오리진 요청을 열어줘야함 (포트 주의)
                 .allowedMethods("*") // http 모든 메소드 요청 허용
                 .allowedHeaders("*") // 헤더 정보 모두 허용
                 .allowCredentials(true); // 쿠키, 세션 정보도 허용
