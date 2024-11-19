8354b531e9ca0af0f97414d4e9a303a454312623
ImJiyun <jiyunk920@gmail.com>
2024-11-19 14:36:09 +0900

    feat: allow CORS globally

--- src/main/java/com/ssafy/fitpass/config/WebConfig.java
+++ src/main/java/com/ssafy/fitpass/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.fitpass.config;
 
 import com.ssafy.fitpass.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/users/logout")
                 .excludePathPatterns("/api/users/emailCheck")
                 .excludePathPatterns("/api/users/nnCheck");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 오리진 요청을 열어줘야함 (포트 주의)
+                .allowedMethods("*") // http 모든 메소드 요청 허용
+                .allowedHeaders("*") // 헤더 정보 모두 허용
+                .allowCredentials(true); // 쿠키, 세션 정보도 허용
+
     }
 }
