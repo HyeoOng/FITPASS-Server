8b8f665e02145d3bf8980a51e905f7295e5c1eca
나혜원 <n417759@gmail.com>
2024-11-21 14:33:24 +0900

    fix: fix photo controller

--- src/main/java/com/ssafy/fitpass/photo/PhotoController.java
+++ src/main/java/com/ssafy/fitpass/photo/PhotoController.java
@@ -1,14 +1,19 @@
 package com.ssafy.fitpass.photo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,11 +23,50 @@ import java.nio.file.Paths;
 //@CrossOrigin(origins = "http://localhost:5173")
 public class PhotoController {
 
-    static private final Path BASE_PATH = Paths.get("src/webapp/WEB-INF").toAbsolutePath().normalize();;
+    static private final String BASE_PATH = "src/main/webapp/WEB-INF";
+
+//    @PostMapping
+//    public ResponseEntity<?> displayPhoto(@RequestBody String photoUrlJson) {
+//        ObjectMapper mapper = new ObjectMapper();
+//        String photoUrl = "";
+//
+//        try{
+//            JsonNode jsonNode = mapper.readTree(photoUrlJson);
+//            photoUrl = jsonNode.get("photoUrl").asText();
+//        } catch (JsonMappingException e) {
+//            throw new RuntimeException(e);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        System.out.println("입력받은 값: " + photoUrl);
+//
+//        Path file = Paths.get(BASE_PATH, photoUrl).normalize();
+//        System.out.println("파일이 존재하는지 확일한 위치: " + file.toAbsolutePath().toString());
+//        if (!file.startsWith(BASE_PATH)) {
+//            System.out.println("경로를 탈출했어요.");
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("hacker"); // 경로 탈출 방지
+//        }
+//        Resource resource = new FileSystemResource(file.toAbsolutePath().toString());
+//
+//        if(!resource.exists()){
+//            System.out.println("파일을 찾지 못했어요.");
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("not found");
+//        }
+//        System.out.println("파일을 찾았습니다!!!!!!!!!!!!!!!!");
+//        HttpHeaders headers = new HttpHeaders();
+//        try {
+//            headers.add("Content-Type", Files.probeContentType(file));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("완료");
+//        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+//
+//    }
 
     @PostMapping
     public ResponseEntity<?> getImage(@RequestBody String imageUrlJson){
-//        System.out.println("photoUrl : "+imageUrl);
 
         // keywordJson에서 "keyword" 값을 추출
         ObjectMapper keywordMapper = new ObjectMapper();
@@ -39,34 +83,28 @@ public class PhotoController {
             // 2. normalize : 경로에서 불필요하거나 비정상적인 요소를 제거하여 정규화된 경로를 반환
             // 3. startsWith : 결과가 BASE_PATH를 벗어난 경로라면 일을 수행하지 못하도록 막는다.
 //            Path file = BASE_PATH.resolve(Paths.get(imageUrl)).normalize();
-            Path file = Paths.get(BASE_PATH+imageUrl).normalize();
-            System.out.println("base_path"+ BASE_PATH);
-            System.out.println("file: "+file.toAbsolutePath());
+            Path file = Paths.get(BASE_PATH, imageUrl).normalize();
             if (!file.startsWith(BASE_PATH)) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("hacker"); // 경로 탈출 방지
             }
-//            Resource resource = new UrlResource(file.toUri());
-            if(Files.exists(file)){System.out.println("일단 파일은 존재해요.");}
-            else {
-                System.out.println("존재하지 않아요..");
-            }
-
+            Resource resource = null;
             if (Files.exists(file) && Files.isReadable(file)) {
-                System.out.println("readable");
-                Resource resource = new UrlResource(file.toUri());
-                return ResponseEntity.ok().body(resource);
+                System.out.println("어떤 경로일려나..: " + file.toUri());
+                resource = new UrlResource(file.toUri());
+                HttpHeaders headers = new HttpHeaders();
+                try {
+                    headers.add("Content-Type", Files.probeContentType(file));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("잘 반환함");
+                return new ResponseEntity<>(resource, headers, HttpStatus.OK);
             } else {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server1");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("not found");
             }
-
-//            if (resource.exists() || resource.isReadable()) {
-//                return ResponseEntity.ok().body(resource);
-//            }else{
-//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server1");
-//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server2");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server");
     }
 }
